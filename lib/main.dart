af5a3b27dd56305ac65d811dd4d55be2842322f3
Michael Alisson <michaelalisson698@gmail.com>
2024-04-04 08:20:33 -0300

    version 0.4

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.blue),
+      theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.blue,
-            title: const Text('Projeto Git'),
-            titleTextStyle: const TextStyle(color: Colors.white, fontSize: 30)),
-        body: Container(
-          
-
+          backgroundColor: const Color.fromARGB(255, 46, 45, 45),
+          title: const Text('Projeto Git'),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 30),
+          leading: Builder(
+            // Garantindo o contexto correto
+            builder: (context) => GestureDetector(
+              // Detectando o toque
+              onTap: () => Scaffold.of(context).openDrawer(),
+              child: Container(
+                child: Icon(Icons.menu),
+                color: const Color.fromARGB(255, 46, 45, 45),
+              ),
+            ),
+          ),
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.white70,
+          child: ListView(
+            padding:
+                EdgeInsets.zero, //REMOVER O PREENCHIMENTO AO REDOR DA LISTA
+            children: const <Widget>[
+              SizedBox(
+                height: 100, //DEFINI ALTURA DO DRAWHEADER
+                child: DrawerHeader(
+                  child: Text(
+                    'Menu',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+              ),
+              ListTile(
+                  title: Text('Configurações'), leading: Icon(Icons.settings)),
+            ],
+          ),
         ),
       ),
     );
